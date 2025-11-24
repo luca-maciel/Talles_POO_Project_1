1ea1c761916835421e1d256b42669f17ac8aa9ae
Gabriela Rodrigues <109330609+bagirodrigues@users.noreply.github.com>
2025-11-24 18:25:33 -0300

    Atualizar o Main.java

--- src/main/java/controle/estoque/Main.java
+++ src/main/java/controle/estoque/Main.java
@@ -1,12 +1,27 @@
 package controle.estoque;
 
-import controle.estoque.entitites.Produto;
+import controle.estoque.entitites.Fornecedor;
 import controle.estoque.menu.Menu;
+import controle.estoque.repositories.MovimentacaoRepository;
+import controle.estoque.repositories.ProdutoRepository;
+import controle.estoque.services.EstoqueService;
+import controle.estoque.utils.ArquivoUtils;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main...");
+
+        ArrayList<Fornecedor> fornecedores =
+                ArquivoUtils.lerFornecedores("fornecedores.csv");
+
+        System.out.println("Fornecedores carregados: " + fornecedores.size());
+
+        ProdutoRepository produtoRepo = new ProdutoRepository();
+        MovimentacaoRepository movRepo = new MovimentacaoRepository();
+        EstoqueService estoqueService = new EstoqueService(produtoRepo, movRepo);
+
+        Menu menu = new Menu(produtoRepo, movRepo, estoqueService);
+        menu.iniciar();
     }
-
-
-
 }
