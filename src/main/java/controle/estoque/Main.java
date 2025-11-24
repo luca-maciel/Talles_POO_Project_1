083d0913b53593b82f3c5333bd0fbe14f17accad
bagirodrigues <109330609+bagirodrigues@users.noreply.github.com>
2025-11-24 19:22:32 -0300

    pra rodar essa bagaça

--- src/main/java/controle/estoque/Main.java
+++ src/main/java/controle/estoque/Main.java
@@ -1,27 +1,16 @@
 package controle.estoque;
 
-import controle.estoque.entitites.Fornecedor;
-import controle.estoque.menu.Menu;
-import controle.estoque.repositories.MovimentacaoRepository;
-import controle.estoque.repositories.ProdutoRepository;
-import controle.estoque.services.EstoqueService;
-import controle.estoque.utils.ArquivoUtils;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main...");
+    }
+}
 
-import java.util.ArrayList;
+/* teste com implementação do Menu:
+import controle.estoque.menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<Fornecedor> fornecedores =
-                ArquivoUtils.lerFornecedores("fornecedores.csv");
-
-        System.out.println("Fornecedores carregados: " + fornecedores.size());
-
-        ProdutoRepository produtoRepo = new ProdutoRepository();
-        MovimentacaoRepository movRepo = new MovimentacaoRepository();
-        EstoqueService estoqueService = new EstoqueService(produtoRepo, movRepo);
-
-        Menu menu = new Menu(produtoRepo, movRepo, estoqueService);
-        menu.iniciar();
+        new Menu().iniciar();
     }
-}
+} */
