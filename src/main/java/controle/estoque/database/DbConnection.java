c4c5c9d0a0f6e023c8a95fc071749e6c526c297d
Gabriela Rodrigues <109330609+bagirodrigues@users.noreply.github.com>
2025-11-24 18:14:03 -0300

    Atualizar o DbConnection.java

--- src/main/java/controle/estoque/database/DbConnection.java
+++ src/main/java/controle/estoque/database/DbConnection.java
@@ -1,29 +1,20 @@
-// Main criado para testar a conexão com o banco
-
 package controle.estoque.database;
-import controle.estoque.exceptions.DbConnectionErrorException;
 
+import controle.estoque.exceptions.DbConnectionErrorException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbConnection {
-    // Muda para a URL do banco em questão
+
     private static final String URL = "jdbc:postgresql://localhost:5432/teste";
-    // Muda para o user do banco
     private static final String USER = "postgres";
-    // Senha do user do banco
     private static final String PASSWORD = "7695";
 
-    public static Statement getConnection() {
+    public static Connection getConnection() {
         try {
-            try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
-                Statement st = conn.createStatement();
-                return st;
-            }
-        }
-        catch (SQLException e) {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
             throw new DbConnectionErrorException("Erro ao conectar ao banco");
         }
     }
